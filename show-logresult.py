bf9820e17b6c2b83971ee26abd27732666ebecf2
yaqiang <sunyaking@163.com>
2020-07-27 15:52:42 +0800

    upload code, modify by other project

---
+++ show-logresult.py
@@ -0,0 +1,22 @@
+import argparse
+
+import torch
+from torch.utils.data import DataLoader
+
+from mini_imagenet import MiniImageNet
+from samplers import CategoriesSampler
+from convnet import Convnet
+from utils import pprint, set_gpu, count_acc, Averager, euclidean_metric
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu', default='0')
+    parser.add_argument('--load', default='./save/proto-1/max-acc.pth')
+    parser.add_argument('--batch', type=int, default=2000)
+    parser.add_argument('--way', type=int, default=5)
+    parser.add_argument('--shot', type=int, default=1)
+    parser.add_argument('--query', type=int, default=30)
+    args = parser.parse_args()
+    result = torch.load('./save/proto-1/trlog')
+    print(result)
